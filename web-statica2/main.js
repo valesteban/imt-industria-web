dab59cb861ebb04b79df8cd1e206e2455000b027
valesteban <valentina.esteban@ug.uchile.cl>
2024-12-27 00:37:42 -0300

    Avance navmenu, footer y imagen comienzo

---
+++ web-statica2/main.js
@@ -0,0 +1,58 @@
+(function() {
+    "use strict";
+
+    /**
+     * Apply .scrolled class to the body as the page is scrolled down
+     */
+    function toggleScrolled() {
+        const selectBody = document.querySelector('body');
+        const selectHeader = document.querySelector('#header');
+        if (!selectHeader.classList.contains('scroll-up-sticky') && !selectHeader.classList.contains('sticky-top') && !selectHeader.classList.contains('fixed-top')) return;
+        window.scrollY > 100 ? selectBody.classList.add('scrolled') : selectBody.classList.remove('scrolled');
+    }
+
+    document.addEventListener('scroll', toggleScrolled);
+
+    /**
+     * Mobile nav toggle
+     */
+    const mobileNavToggleBtn = document.querySelector('.mobile-nav-toggle');
+
+    function mobileNavToogle() {
+        document.querySelector('body').classList.toggle('mobile-nav-active');
+        mobileNavToggleBtn.classList.toggle('bi-list');
+        mobileNavToggleBtn.classList.toggle('bi-x');
+    }
+    mobileNavToggleBtn.addEventListener('click', mobileNavToogle);
+
+    /**
+     * Hide mobile nav on same-page/hash links
+     */
+    document.querySelectorAll('#navmenu a').forEach(navmenu => {
+        navmenu.addEventListener('click', () => {
+            if (document.querySelector('.mobile-nav-active')) {
+                mobileNavToogle();
+            }
+        });
+    });
+
+    /**
+     * Init swiper sliders
+     */
+    function initSwiper() {
+        document.querySelectorAll(".init-swiper").forEach(function(swiperElement) {
+            let config = JSON.parse(
+                swiperElement.querySelector(".swiper-config").innerHTML.trim()
+            );
+
+            if (swiperElement.classList.contains("swiper-tab")) {
+                initSwiperWithCustomPagination(swiperElement, config);
+            } else {
+                new Swiper(swiperElement, config);
+            }
+        });
+    }
+
+    window.addEventListener("load", initSwiper);
+
+})();
